26dd74499610d5a5bf5d9d6980d8798a43560d26
darkTeaLeaf <arina.fedorovskaya@gmail.com>
2019-05-19 21:44:49 +0300

    deploy

--- kanban_board/settings.py
+++ kanban_board/settings.py
@@ -127,10 +127,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'board/static')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'staticfiles'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'board/static'),
+# )
 
